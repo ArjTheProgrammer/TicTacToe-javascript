7933a8e1d7850457527b9c57183f0728947b8e11
Jose Neil Silagan <neilsilagan@gmail.com>
2024-07-06 20:14:51 +0800

    Make a player function and alternate the marker from X to O

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function Gameboard(){
     const grids = document.querySelectorAll(".grid");
-    
+
+    const playerX = player("Rj", "X");
+    const playerO = player("Jear", "O");
+    let currentPlayer = playerX;
+
     grids.forEach(grid => {
         grid.addEventListener(("click"), () =>{
-            grid.textContent = "X";
+            grid.textContent = currentPlayer == playerX ? playerX.marker : playerO.marker;
+            currentPlayer = currentPlayer == playerO ? playerX : playerO;
         })
-    })
+    });
+}
+
+function player(name, marker){
+return {name, marker}
 }
 
 Gameboard();
