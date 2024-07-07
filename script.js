f14ec20141f0fb11f8391cd761d2004e6003deba
Jose Neil Silagan <neilsilagan@gmail.com>
2024-07-07 18:55:49 +0800

    Add the postgame dialog and fixed a bug
    The bug is when the restart button is clicked, stills checks and identifies the winner even if the placedMarked of each player has been cleared

--- script.js
+++ script.js
@@ -1,77 +1,104 @@
 function Gameboard(){
-    const showInputName = document.querySelector("dialog");
+    const showInputName = document.querySelector(".inputName");
+    const showPostgame = document.querySelector(".postgame");
     const grids = document.querySelectorAll(".grid");
     const playerXField = document.querySelector(".player-x");
     const playerOField = document.querySelector(".player-o");
     const playerXname = document.querySelector("#player-x-name");
     const playerOname = document.querySelector("#player-o-name");
+    const announcement = document.querySelector(".announcement");
     const playButton = document.querySelector(".play");
+    const restartButton = document.querySelector(".restart");
 
-        showInputName.showModal();
+    let playerX;
+    let playerO;
+    let currentPlayer;
+    let turns = 0;
+
+    showInputName.showModal();
 
     playButton.addEventListener("click", () => {
         const xDiv = document.createElement("div");
         const oDiv = document.createElement("div");
 
-        xDiv.textContent = playerXname.value;
-        oDiv.textContent = playerOname.value;
+        xDiv.textContent = `X: ${playerXname.value}`;
+        oDiv.textContent = `O: ${playerOname.value}`;
         console.log(`${playerXname.value} ${playerOname.value}`)
+        playerX = player(`${playerXname.value}`, "X");
+        playerO = player(`${playerOname.value}`, "O");
+        currentPlayer = playerX;
 
         playerXField.appendChild(xDiv);
         playerOField.appendChild(oDiv);
         showInputName.close();
-    })
+    });
 
     function player(name, marker){
         let placedMarked = ['','','','','','','','',''];
 
         function checkWinner(){
+            console.log(placedMarked);
             //horizontal
             if ((placedMarked[0] == `${currentPlayer.marker}` && placedMarked[1] == `${currentPlayer.marker}` && placedMarked[2] == `${currentPlayer.marker}`)||
                 (placedMarked[3] == `${currentPlayer.marker}` && placedMarked[4] == `${currentPlayer.marker}` && placedMarked[5] == `${currentPlayer.marker}`) ||
                 (placedMarked[6] == `${currentPlayer.marker}` && placedMarked[7] == `${currentPlayer.marker}` && placedMarked[8] == `${currentPlayer.marker}`)
             ){
-                return console.log(`Winner is ${currentPlayer.name}`);
+                console.log(`${currentPlayer.name} won!`);
+                announcement.textContent = `${currentPlayer.name} won!`;
+                showPostgame.showModal();
             }
 
-            //horizontal
-            if ((placedMarked[0] == `${currentPlayer.marker}` && placedMarked[3] == `${currentPlayer.marker}` && placedMarked[6] == `${currentPlayer.marker}`)||
+            //horizontal    
+            else if ((placedMarked[0] == `${currentPlayer.marker}` && placedMarked[3] == `${currentPlayer.marker}` && placedMarked[6] == `${currentPlayer.marker}`)||
                 (placedMarked[1] == `${currentPlayer.marker}` && placedMarked[4] == `${currentPlayer.marker}` && placedMarked[7] == `${currentPlayer.marker}`) ||
                 (placedMarked[2] == `${currentPlayer.marker}` && placedMarked[5] == `${currentPlayer.marker}` && placedMarked[8] == `${currentPlayer.marker}`)
             ){
-                return console.log(`Winner is ${currentPlayer.name}`);
+                console.log(`${currentPlayer.name} won!`);
+                announcement.textContent = `${currentPlayer.name} won!`;
+                showPostgame.showModal();
             }
 
             //diagonal
-            if ((placedMarked[0] == `${currentPlayer.marker}` && placedMarked[4] == `${currentPlayer.marker}` && placedMarked[8] == `${currentPlayer.marker}`)||
+            else if ((placedMarked[0] == `${currentPlayer.marker}` && placedMarked[4] == `${currentPlayer.marker}` && placedMarked[8] == `${currentPlayer.marker}`)||
                 (placedMarked[2] == `${currentPlayer.marker}` && placedMarked[4] == `${currentPlayer.marker}` && placedMarked[6] == `${currentPlayer.marker}`)
             ){
-                return console.log(`Winner is ${currentPlayer.name}`);
+                console.log(`${currentPlayer.name} won!`);
+                announcement.textContent = `${currentPlayer.name} won!`;
+                showPostgame.showModal();
             }
 
             else if (turns == 9){
-                return console.log("tie!");
+                announcement.textContent = `Tied!`;
+                showPostgame.showModal();
             }
         }
-        return {name, marker, placedMarked,checkWinner};
+        return {name, marker, placedMarked, checkWinner};
         }
-    
-    const playerX = player("playerX", "X");
-    const playerO = player("playerO", "O");
-    let currentPlayer = playerX;
-    let turns = 0;
 
     grids.forEach((grid, index) => {
         grid.addEventListener(("click"), () =>{
             if (grid.textContent === ""){
+                console.log(`${currentPlayer.name}`)
                 turns++;
                 currentPlayer.placedMarked.splice(index, 1, currentPlayer.marker);
+                console.log(currentPlayer.placedMarked);
                 grid.textContent = currentPlayer.marker;
                 currentPlayer.checkWinner();
                 currentPlayer = currentPlayer == playerO ? playerX : playerO;
             }
-        });   
-    });
-}
+        });
+    }); 
+
+            
+    restartButton.addEventListener("click", () => {
+        grids.forEach(grid => {
+            grid.textContent = "";
+        })
+        playerX = player(`${playerXname.value}`, "X");
+        playerO = player(`${playerOname.value}`, "O");
+        currentPlayer = playerX;
+        turns = 0;
+    })
+}   
 
 Gameboard();
